390404ec5974d5fc348d175c019c8a0c3f190bdf
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-05-05 14:53:27 -0700

    Continue working wit hSolana Pay on project

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('H455qminGRaaQpC9Z6RS1CyfTB5jupLSwrUthXzkAsSV') 
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
