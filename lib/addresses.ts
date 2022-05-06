3ea1f0dcfcf714a38414e34c2160342430da5100
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-05-06 01:42:45 -0700

    Finish basic Solana Pay structure

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -4,3 +4,5 @@ import { PublicKey } from "@solana/web3.js"
 export const shopAddress = new PublicKey('H455qminGRaaQpC9Z6RS1CyfTB5jupLSwrUthXzkAsSV') 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('74aFyMgbKppdab4wEVz5AXL3UGbxMkwkwgK2LVzn3MmG')
